2f22beb7da130164e5938483799d3b0198cdfb65
Trevor <valentinosolischang@gmail.com>
2025-03-09 16:12:08 -0500

    Wrap App component with ModalProvider for modal state management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import Chat from "./components/Chat";
 import Comment from "./components/Comment";
 import { CommentsProvider } from "./contexts/CommentsProvider";
+import ModalProvider from "./contexts/ModalProvider";
 
 function App() {
   return (
-    <CommentsProvider>
-      <Comment />
-      <Chat />
-    </CommentsProvider>
+    <ModalProvider>
+      <CommentsProvider>
+        <Comment />
+        <Chat />
+      </CommentsProvider>
+    </ModalProvider>
   );
 }
 
